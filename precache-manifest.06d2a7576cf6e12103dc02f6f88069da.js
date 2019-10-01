38486b9db917be032d937cbc9282c3bf7204a981
Peter Boyer <petertboyer@gmail.com>
2019-10-01 17:26:09 +1000

    Updates

---
+++ precache-manifest.06d2a7576cf6e12103dc02f6f88069da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b2c84f3e679e9a1a4fb3f8f4eaa1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a513e5c7efe89dacc5bc",
+    "url": "/static/css/main.68de15b1.chunk.css"
+  },
+  {
+    "revision": "764aa9df9389636c4081",
+    "url": "/static/js/2.d5f4df26.chunk.js"
+  },
+  {
+    "revision": "a513e5c7efe89dacc5bc",
+    "url": "/static/js/main.4d0234cb.chunk.js"
+  },
+  {
+    "revision": "f6ffaccbef74c4dc7aec",
+    "url": "/static/js/runtime-main.c391e37c.js"
+  },
+  {
+    "revision": "3345769b6d292096ed1d506f2e14d565",
+    "url": "/static/media/bg-5.3345769b.jpg"
+  },
+  {
+    "revision": "0a0969cab1ed244bfcabe91c48dd1174",
+    "url": "/static/media/facebook.0a0969ca.svg"
+  },
+  {
+    "revision": "3271582d2c0b0a6fe3d1b8462e6f2e93",
+    "url": "/static/media/instagram.3271582d.svg"
+  },
+  {
+    "revision": "c1e4f70398a0ed2e936c34edbdcad685",
+    "url": "/static/media/logo.c1e4f703.png"
+  },
+  {
+    "revision": "863ac30d4dfdb3a624543a92c49a0dfe",
+    "url": "/static/media/topic-feed.863ac30d.png"
+  },
+  {
+    "revision": "2cfee916be18ece657da8dcd88eb7716",
+    "url": "/static/media/topic-friends.2cfee916.png"
+  },
+  {
+    "revision": "dc280e9c9e28555863a9ea98471624d2",
+    "url": "/static/media/topic-venues.dc280e9c.png"
+  }
+]);
